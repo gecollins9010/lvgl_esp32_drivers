7dceeabfa04a97786458bfb359f83276ac03501e
Greg Collins <gecollins356@gmail.com>
2026-05-07 20:58:28 -0400

    Modified files from Hosyond for E32R32P

--- lvgl_helpers.h
+++ lvgl_helpers.h
@@ -22,6 +22,12 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
+#ifndef LV_HOR_RES_MAX
+#define LV_HOR_RES_MAX (320)
+#endif
+#ifndef LV_VER_RES_MAX
+#define LV_VER_RES_MAX (240)
+#endif
 
 /* DISP_BUF_SIZE value doesn't have an special meaning, but it's the size
  * of the buffer(s) passed to LVGL as display buffers. The default values used
--- lvgl_tft/st7789.c
+++ lvgl_tft/st7789.c
@@ -51,42 +51,33 @@ static void st7789_send_color(void *data, size_t length);
 void st7789_init(void)
 {
     lcd_init_cmd_t st7789_init_cmds[] = {
-        {0xCF, {0x00, 0x83, 0X30}, 3},
-        {0xED, {0x64, 0x03, 0X12, 0X81}, 4},
-        {ST7789_PWCTRL2, {0x85, 0x01, 0x79}, 3},
-        {0xCB, {0x39, 0x2C, 0x00, 0x34, 0x02}, 5},
-        {0xF7, {0x20}, 1},
-        {0xEA, {0x00, 0x00}, 2},
-        {ST7789_LCMCTRL, {0x26}, 1},
-        {ST7789_IDSET, {0x11}, 1},
-        {ST7789_VCMOFSET, {0x35, 0x3E}, 2},
-        {ST7789_CABCCTRL, {0xBE}, 1},
-        {ST7789_MADCTL, {0x00}, 1}, // Set to 0x28 if your display is flipped
-        {ST7789_COLMOD, {0x55}, 1},
-
+        {ST7789_MADCTL, {0x00}, 1},
+        {ST7789_COLMOD, {0x05}, 1},
+        {0xB2, {0x0C, 0x0C, 0X00, 0x33, 0x33}, 5},
+        {ST7789_GCTRL, {0x74}, 1},
+        {0xBB, {0x23}, 1},
+        {ST7789_LCMCTRL, {0x2C}, 1},
+        {0xC2, {0x01}, 1},
+        {0xC3, {0x10}, 1},
+        {0xC4, {0x20}, 1},
+        {0xC6, {0x0F}, 1},
+        {0xD0, {0xA4, 0xA1}, 2},
+        {0xD6, {0xA1}, 1},
+        {ST7789_PVGAMCTRL, {0xD0, 0x07, 0x0E, 0x0B, 0x0A, 0x14, 0x38, 0x33, 0x4F, 0x37, 0x16, 0x16, 0x2A, 0x2E}, 14},
+        {ST7789_NVGAMCTRL, {0xD0, 0x0B, 0x10, 0x08, 0x08, 0x06, 0x35, 0x54, 0x4D, 0x0A, 0x14, 0x14, 0x2C, 0x2F}, 14},
+        {0xE9, {0x11, 0x11, 0x03}, 3},        
 #if ST7789_INVERT_COLORS == 1
 		{ST7789_INVON, {0}, 0}, // set inverted mode
 #else
  		{ST7789_INVOFF, {0}, 0}, // set non-inverted mode
 #endif
-
-        {ST7789_RGBCTRL, {0x00, 0x1B}, 2},
-        {0xF2, {0x08}, 1},
-        {ST7789_GAMSET, {0x01}, 1},
-        {ST7789_PVGAMCTRL, {0xD0, 0x00, 0x02, 0x07, 0x0A, 0x28, 0x32, 0x44, 0x42, 0x06, 0x0E, 0x12, 0x14, 0x17}, 14},
-        {ST7789_NVGAMCTRL, {0xD0, 0x00, 0x02, 0x07, 0x0A, 0x28, 0x31, 0x54, 0x47, 0x0E, 0x1C, 0x17, 0x1B, 0x1E}, 14},
-        {ST7789_CASET, {0x00, 0x00, 0x00, 0xEF}, 4},
-        {ST7789_RASET, {0x00, 0x00, 0x01, 0x3f}, 4},
-        {ST7789_RAMWR, {0}, 0},
-        {ST7789_GCTRL, {0x07}, 1},
-        {0xB6, {0x0A, 0x82, 0x27, 0x00}, 4},
         {ST7789_SLPOUT, {0}, 0x80},
         {ST7789_DISPON, {0}, 0x80},
         {0, {0}, 0xff},
     };
 
     //Initialize non-SPI GPIOs
-    gpio_pad_select_gpio(ST7789_DC);
+    esp_rom_gpio_pad_select_gpio(ST7789_DC);
     gpio_set_direction(ST7789_DC, GPIO_MODE_OUTPUT);
 
 #if !defined(ST7789_SOFT_RST)
@@ -112,7 +103,7 @@ void st7789_init(void)
         st7789_send_cmd(st7789_init_cmds[cmd].cmd);
         st7789_send_data(st7789_init_cmds[cmd].data, st7789_init_cmds[cmd].databytes&0x1F);
         if (st7789_init_cmds[cmd].databytes & 0x80) {
-                vTaskDelay(100 / portTICK_RATE_MS);
+                vTaskDelay(100 / portTICK_PERIOD_MS);
         }
         cmd++;
     }
@@ -228,7 +219,7 @@ static void st7789_set_orientation(uint8_t orientation)
 #if CONFIG_LV_PREDEFINED_DISPLAY_TTGO
 	0x60, 0xA0, 0x00, 0xC0
 #else
-	0xC0, 0x00, 0x60, 0xA0
+	0x00, 0xC0, 0x60, 0xA0
 #endif
     };
 
